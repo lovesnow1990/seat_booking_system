b41986ffd4755d63ba0232138d144168376dd929
anthonylu <lovesnow1990@gmail.com>
2025-07-08 00:25:16 +0800

    自動生成log資料夾

--- seat_booking_system_backend/settings.py
+++ seat_booking_system_backend/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+os.makedirs(os.path.join(BASE_DIR, 'logs'), exist_ok=True)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
